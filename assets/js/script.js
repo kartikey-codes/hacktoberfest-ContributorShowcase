71f83f014ad4443b711377bbbc215061669147b7
Kartikey Bhatnagar <kartikey.bhatnagar2021@vitstudent.ac.in>
2023-09-30 16:47:33 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 20, 
+                behavior: 'smooth'
+            });
+        }
+    });
+});
